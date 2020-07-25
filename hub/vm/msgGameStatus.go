a4e39d933835574140180a1873f9635be2cb7ee0
Lilla Imry <limry@mi-i3.msk.21-school.ru>
2020-07-25 14:34:17 +0300

    tcp

--- hub/vm/msgGameStatus.go
+++ hub/vm/msgGameStatus.go
@@ -9,10 +9,6 @@ import (
 
 const ArenaSize = 4096
 
-type Carriage struct {
-	ProcId  uint32
-	ProcPos uint16
-}
 
 type GameStatus struct {
 	Type             uint8
@@ -25,7 +21,6 @@ type GameStatus struct {
 	LastLivedPlayer  uint8
 	Arena            [ArenaSize]byte
 	Setting          [ArenaSize]byte
-	Cars             []Carriage
 }
 
 func readGameStatus(conn net.Conn) (*GameStatus, error) {
@@ -43,8 +38,6 @@ func readGameStatus(conn net.Conn) (*GameStatus, error) {
 		return m, e
 	}
 
-	numberOfCarriages := (m.Len - 8209) / 6
-	println("Len is ", m.Len, "Number of carriages is", numberOfCarriages)
 
 	if e = binary.Read(conn, binary.BigEndian, &m.CyclesOfDeath); e != nil {
 		return m, e
@@ -67,25 +60,7 @@ func readGameStatus(conn net.Conn) (*GameStatus, error) {
 	if n, e = conn.Read(m.Setting[:]); n != ArenaSize || e != nil {
 		return m, fmt.Errorf("Error reading setting field (%d/%d): %v ", n, ArenaSize, e)
 	}
-	for i := uint32(0); i < numberOfCarriages; i++ {
-		c := Carriage{uint32(0), uint16(0)}
-		if e = binary.Read(conn, binary.BigEndian, &c.ProcId); e != nil {
-			return m, e
-		}
-		if e = binary.Read(conn, binary.BigEndian, &c.ProcPos); e != nil {
-			return m, e
-		}
-		m.Cars = append(m.Cars, c)
-	}
 	return m, e
-}
-
-func ExpandCars(cars []Carriage) string {
-	var str strings.Builder
-	for _, c := range cars {
-		fmt.Fprintf(&str, "{procId:%v, procPos:%v},", c.ProcId, c.ProcPos)
-	}
-	return str.String()
 }
 
 func handleGameStatus(conn net.Conn) error {
@@ -104,14 +79,13 @@ func handleGameStatus(conn net.Conn) error {
 	// TODO: Write game on web site
 	fmt.Printf("Hub Write game on web site "+
 		"{id: %d; len: %d, death: %d, cycles: %d, "+
-		"livesOps: %d, checks: %d, llp: %d,procs:[%s]}\n",
+		"livesOps: %d, checks: %d, llp: %d,\n",
 		msg.Id,
 		msg.Len,
 		msg.CyclesOfDeath,
 		msg.CyclesDone,
 		msg.NumberOfLivesOps,
 		msg.NumberOfChecks,
-		msg.LastLivedPlayer,
-		ExpandCars(msg.Cars))
+		msg.LastLivedPlayer)
 	return err
 }
