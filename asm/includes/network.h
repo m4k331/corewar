7ede4e859aa524525dabd218aabbc51aac5e468c
Александр Полищук <alexander@MacBook-Pro-Aleksandr.local>
2020-07-25 09:44:23 +0300

    added network

--- asm/includes/network.h
+++ asm/includes/network.h
@@ -6,11 +6,73 @@
 /*   By: rnarbo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/21 18:24:18 by rnarbo            #+#    #+#             */
-/*   Updated: 2020/06/21 18:25:18 by rnarbo           ###   ########.fr       */
+/*   Updated: 2020/07/01 00:08:07 by rnarbo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef NETWORK_H
 # define NETWORK_H
+
+# define THREADS				4
+# define RECV_BUFF_SIZE			1024
+
+# define MAX_CONNECTIONS		4
+# define TCP_QUEUE_SIZE 		4
+
+# define HUB_HEADER_SIZE		1
+# define HANDSHAKE_MSG_LEN		5
+# define ASM_MSG_HEADER_LEN		9
+
+# define HANDSHAKE_MAGIC_OFFSET	1
+# define ASM_MSG_LEN_OFFSET		5
+# define ASM_BOT_ID_OFFSET		1
+
+# define HUB_COMMAND_HANDSHAKE	101
+# define HUB_COMMAND_SEND_BOT	3
+
+# define ASM_COMMAND_HANDSHAKE	101
+# define ASM_COMMAND_ERROR		102
+# define ASM_COMMAND_BINARY		103
+
+# define HANDSHAKE_MAGIC		0xFAF
+
+# define THREAD_IN_USE			1
+# define THREAD_FREE			0
+# define THREAD_FINISHED		-1
+
+# define DEFAULT_HUB_PORT		8008
+
+# define USAGE_START "Usage: "
+# define USAGE_ARGS	\
+	" [-h] < -i hub_ip [-p hub_port] | champ_filename >\n"
+
+typedef struct s_net_thread_param
+{
+	int		fd;
+	char	thread_in_use;
+	void	*data;
+}	t_net_thread_param;
+
+typedef struct s_hub_msg
+{
+	char	msg_type;
+	void	*content;
+}	t_hub_msg;
+
+typedef struct s_asm_msg
+{
+	int bot_id;
+	int len_msg;
+	char *body;
+}	t_asm_msg;
+
+typedef struct s_thread_return
+{
+	int		error_code;
+	char	*error_msg;
+}	t_thread_return;
+
+int	lexer_for_net(t_data *data, char *buff, size_t buff_size);
+int	network(t_options *opt);
 
 #endif
--- asm/srcs/main.c
+++ asm/srcs/main.c
@@ -6,12 +6,13 @@
 /*   By: rnarbo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/27 17:51:45 by dorphan           #+#    #+#             */
-/*   Updated: 2020/06/21 19:12:33 by rnarbo           ###   ########.fr       */
+/*   Updated: 2020/07/01 21:20:35 by rnarbo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "asm.h"
 #include "network.h"
+#include <stdio.h> // TODO: remove
 
 int		main_old(int ac, char **av)
 {
@@ -19,8 +20,6 @@ int		main_old(int ac, char **av)
 		ft_putstr(USAGE);
 	else if (ac > 2)
 		ft_putstr_fd(ERROR_MANY_ARGS, 2);
-	// FIXME: ac == 2 избыточно, тк уже были проверки на ac > 2 и ac == 1
-	//FIXED
 	else if (check_file_name(av[1]))
 		compile(av[1]);
 	else
@@ -28,34 +27,106 @@ int		main_old(int ac, char **av)
 	return (0);
 }
 
-// Заглушка
+int usage(int fd, char *prog_name)
+{
+	write(fd, USAGE_START, sizeof(USAGE_START));
+	ft_putstr_fd(prog_name, fd);
+	write(fd, USAGE_ARGS, sizeof(USAGE_ARGS));
+	return (0);
+}
+
+int invalid_opt(char *opt)
+{
+	write(2, "Invalid option: \"", sizeof("Invalid option: \""));
+	ft_putendl_fd(opt, 2);
+	return (0);
+}
+
+int ft_is_number(char *str)
+{
+	int i;
+	int dig_was_found;
+
+	i = 0;
+	while (ft_isspace(str[i]) && str[i])
+		i++;
+	dig_was_found = 0;
+	while (ft_isdigit(str[i]) && str[i])
+	{
+		dig_was_found = 1;
+		i++;
+	}
+	while (ft_isspace(str[i]) && str[i])
+		i++;
+	if (str[i] != '\0' || !dig_was_found)
+		return (0);
+	return (1);
+}
+
+int invalid_port_number(char *port)
+{
+	write(2, "Invalid port number: \"", sizeof("Invalid port number: \""));
+	ft_putstr_fd(port, 2);
+	write(2, "\"\n", 2);
+	return (0);
+}
+
 int	get_options(t_options *opt, int argc, char **argv)
 {
-	if (argc == 1)
-		return (1);
-	else if (argc > 2)
-		ft_putstr_fd(ERROR_MANY_ARGS, 2);
-	else
-	{
-		opt->filename = argv[1];
-		return (0);
-	}
-	return (1);
+	int i;
+
+	i = 0;
+	if (argc == 2 && ft_strcmp(argv[1], "-h"))
+		opt->filename = argv[++i];
+	while (++i < argc)
+		if (argv[i][0] == '-')
+		{
+			if (argv[i][1] == 'h' && argv[i][2] == '\0')
+				exit(usage(1, argv[0]));
+			else if (i + 1 >= argc)
+				return (1);
+			if (argv[i][1] == 'i' && argv[i][2] == '\0')
+				opt->ip = argv[i++ + 1];
+			else if (argv[i][1] == 'p' && argv[i][2] == '\0')
+			{
+				if (!ft_is_number(argv[i + 1]))
+					return (1 | invalid_port_number(argv[i + 1]));
+				opt->port = ft_atoi(argv[i++ + 1]);
+			}
+			else
+				return (1 | invalid_opt(argv[i]));
+		}
+		else
+			return (1);
+	return (0);
 }
 
 int checks(t_options *opt, int argc, char **argv)
 {
+	ft_memset(opt, 0, sizeof(t_options));
 	if (get_options(opt, argc, argv))
-		ft_putstr_fd(USAGE, 2);
-	else if (opt->server == 1 || check_file_name(opt->filename) == 0)
-		return (0);
-	return (1);
+		return (1);
+	if (!opt->filename)
+	{
+		if (opt->ip == NULL)
+			return (1);
+		if (opt->port == 0)
+			opt->port = DEFAULT_HUB_PORT;
+	}
+	else if (check_file_name(opt->filename) == 0)
+	{
+		ft_putstr_fd(ERROR_INVALID_CHAMP_NAME, 2);
+		return (1);
+	}
+	return (0);
 }
 
-// Заглушка
-int network(t_options *opt)
+// TODO: remove
+int print_opt(t_options *opt)
 {
-	(void)opt;
+	printf("IP: %s\n", opt->ip);
+	printf("Port: %hd\n", opt->port);
+	printf("Filename: %s\n", opt->filename);
 	return (0);
 }
 
@@ -65,8 +136,10 @@ int main(int argc, char **argv)
 	t_options	opt;
 
 	if (checks(&opt, argc, argv))
-		return (1);
-	if (opt.server)
+		return (1 | usage(2, argv[0]));
+	print_opt(&opt);
+	// return (0);
+	if (opt.filename)
 		return (network(&opt));
 	if ((data = compile(argv[1])) == NULL)
 		return (1);
