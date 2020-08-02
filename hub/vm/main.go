3e1073c4f6befdf17d2953dd7ea8d5c4420d8478
m4k331 <makzzi777@gmail.com>
2020-08-02 07:35:14 +0300

    refactoring start hub server

--- hub/vm/main.go
+++ hub/vm/main.go
@@ -31,6 +31,12 @@ const (
 	TypeMsgBinaryASM
 )
 
+const (
+	startHubServer         = "Start hub server"
+	failedConnToHubServer  = "Failed to connect the service to the hub server"
+	successConnToHubServer = "Service connected to hub server"
+)
+
 func main() {
 	config, err := NewConfig(ConfigPath)
 	if err != nil {
@@ -41,29 +47,17 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	hub.Log.Info("Start hub server",
-		zap.String("addr", hub.Addr),
-	)
+	hub.Log.Info(startHubServer, zap.String("addr", hub.Addr))
 
 	for {
 		service, err := hub.ConnectNewService()
 		if err != nil {
-			hub.Log.Error("Failed to connect the service to the hub server",
-				zap.Error(err),
-			)
+			hub.Log.Error(failedConnToHubServer, zap.Error(err))
 			continue
 		}
 
-		hub.Log.Info("Service connected to hub server",
-			zap.String("addr", service.Addr),
-		)
+		hub.Log.Info(successConnToHubServer, zap.String("addr", service.Addr))
 
 		service.Run()
-		// TODO: delete
-		//go func() {
-		//	timer := time.NewTimer(time.Second * 5)
-		//	<-timer.C
-		//	tmpInitialGame(conn, 1) // n - number champs
-		//}()
 	}
 }
