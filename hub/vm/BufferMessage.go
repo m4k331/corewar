dec002c08174512d0fb31887d9144c868c9de4d1
m4k331 <makzzi777@gmail.com>
2020-08-06 02:35:28 +0300

    fixed: method Resize() in BufferMessage && impl: writer and reader interface in BufferMessage

--- hub/vm/BufferMessage.go
+++ hub/vm/BufferMessage.go
@@ -16,7 +16,9 @@ func (b *BufferMessage) Resize(n int) {
 	switch {
 	case n < 0:
 	case n > cap(b.buff):
-		b.buff = make([]byte, n)
+		fat := make([]byte, n)
+		copy(fat, b.buff[:cap(b.buff)])
+		b.buff = fat
 	default:
 		b.buff = b.buff[:n]
 	}
@@ -41,14 +43,22 @@ func (b *BufferMessage) ReadN(r io.Reader, n int) error {
 }
 
 func (b *BufferMessage) Read(p []byte) (n int, e error) {
+	n = len(p)
+	offset := len(b.buff)
+	b.Resize(offset + n)
+	copy(b.buff[offset:], p)
+	return n, e
+}
+
+func (b *BufferMessage) Write(p []byte) (n int, e error) {
 	switch {
 	case len(p) > len(b.buff):
-		copy(p, b.buff[:])
-		e = io.EOF
 		n = len(b.buff)
+		e = io.EOF
 	default:
-		copy(p, b.buff[:len(p)])
 		n = len(p)
 	}
+	copy(p, b.buff[:n])
+	b.buff = b.buff[n:]
 	return n, e
 }
--- hub/vm/BufferMessage_test.go
+++ hub/vm/BufferMessage_test.go
@@ -7,7 +7,7 @@ import (
 	"testing"
 )
 
-func TestBufferMessageRead(t *testing.T) {
+func TestBufferMessage(t *testing.T) {
 	a := make([]byte, 50)
 	r := bytes.NewBuffer(a)
 	b := NewBufferMessage(50)
@@ -16,6 +16,7 @@ func TestBufferMessageRead(t *testing.T) {
 		b.buff[i] = byte(i)
 	}
 
+	// TEST #1
 	n := 10
 	e := b.ReadN(r, n)
 	if e != nil {
@@ -26,19 +27,21 @@ func TestBufferMessageRead(t *testing.T) {
 		t.Errorf("1. Fail test ReadN: [Read size %d]\ngot:%v\nexp:%v\n", n, b.buff, a)
 	}
 
+	// TEST #2
 	l := n
 	g := make([]byte, l)
-	n, e = b.Read(g)
+	n, e = b.Write(g)
 	if e != nil {
-		t.Errorf("2. Fail test Read: [%v]\n", e)
+		t.Errorf("2. Fail test Write: [%v]\n", e)
 	}
 	if n != l {
-		t.Errorf("2. Fail test Read:\ngot size:%d\nexp size:%d\n", n, l)
+		t.Errorf("2. Fail test Write:\ngot size:%d\nexp size:%d\n", n, l)
 	}
 	if !reflect.DeepEqual(g, a) {
-		t.Errorf("2. Fail test Read:\ngot:%v\nexp:%v\n", g, a)
+		t.Errorf("2. Fail test Write:\ngot:%v\nexp:%v\n", g, a)
 	}
 
+	// TEST #3
 	n = 0
 	a = []byte{}
 	e = b.ReadN(r, n)
@@ -49,19 +52,21 @@ func TestBufferMessageRead(t *testing.T) {
 		t.Errorf("3. Fail test ReadN: [Read size %d]\ngot:%v\nexp:%v\n", n, b.buff, a)
 	}
 
+	// TEST #4
 	l = n
 	g = make([]byte, l)
-	n, e = b.Read(g)
+	n, e = b.Write(g)
 	if e != nil {
-		t.Errorf("4. Fail test Read: [%v]\n", e)
+		t.Errorf("4. Fail test Write: [%v]\n", e)
 	}
 	if n != l {
-		t.Errorf("4. Fail test Read:\ngot size:%d\nexp size:%d\n", n, l)
+		t.Errorf("4. Fail test Write:\ngot size:%d\nexp size:%d\n", n, l)
 	}
 	if !reflect.DeepEqual(g, a) {
-		t.Errorf("4. Fail test Read:\ngot:%v\nexp:%v\n", g, a)
+		t.Errorf("4. Fail test Write:\ngot:%v\nexp:%v\n", g, a)
 	}
 
+	// TEST #5
 	n = 410
 	a = []byte{10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49}
 	e = b.ReadN(r, 410)
@@ -69,17 +74,63 @@ func TestBufferMessageRead(t *testing.T) {
 		t.Errorf("5. Fail test ReadN: [Read size %d]\ngot:%v\nexp:%v\n", n, b.buff, a)
 	}
 
+	// TEST #6
 	l = n
 	g = make([]byte, l)
-	n, e = b.Read(g)
+	n, e = b.Write(g)
 	if e != io.EOF {
-		t.Errorf("6. Fail test Read: [%v]\n", e)
+		t.Errorf("6. Fail test Write: [%v]\n", e)
 	}
 	if n != len(a) {
-		t.Errorf("6. Fail test Read:\ngot size:%d\nexp size:%d\n", n, len(a))
+		t.Errorf("6. Fail test Write:\ngot size:%d\nexp size:%d\n", n, len(a))
 	}
 	a = append(a, make([]byte, l-len(a))...)
 	if !reflect.DeepEqual(g, a) {
-		t.Errorf("6. Fail test Read:\ngot:%v\nexp:%v\n", g, a)
+		t.Errorf("6. Fail test Write:\ngot:%v\nexp:%v\n", g, a)
 	}
+
+	// TEST #7
+	a = []byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+	n, e = b.Read(a)
+	if e != nil {
+		t.Errorf("7. Fail test Read: [%v]\n", e)
+	}
+	if n != len(a) {
+		t.Errorf("7. Fail test Read:\ngot size:%d\nexp size:%d\n", n, len(a))
+	}
+	if !reflect.DeepEqual(b.buff, a) {
+		t.Errorf("7. Fail test Read:\ngot:%v\nexp:%v\n", b.buff, a)
+	}
+
+	// TEST #8
+	n, e = b.Read(a)
+	if e != nil {
+		t.Errorf("8. Fail test Read: [%v]\n", e)
+	}
+	if n != len(a) {
+		t.Errorf("8. Fail test Read:\ngot size:%d\nexp size:%d\n", n, len(a))
+	}
+	a = append(a, a...)
+	if !reflect.DeepEqual(b.buff, a) {
+		t.Errorf("8. Fail test Read:\ngot:%v\nexp:%v\n", b.buff, a)
+	}
+
+	// TEST #9
+	var i int
+
+	for i < 5 {
+		n, e = b.Read(a)
+		if e != nil {
+			t.Errorf("9.%d Fail test Read: [%v]\n", i, e)
+		}
+		if n != len(a) {
+			t.Errorf("9.%d Fail test Read:\ngot size:%d\nexp size:%d\n", i, n, len(a))
+		}
+		a = append(a, a...)
+		i++
+	}
+	if !reflect.DeepEqual(b.buff, a) {
+		t.Errorf("9.%d Fail test Read:\ngot:%v\nexp:%v\n", i, b.buff, a)
+	}
+
 }
